039f88f61cd0fbda39be42b8acaca6636b69375c
Caike Sannto <ctscaike@gmail.com>
2025-08-29 18:41:30 -0300

    Criar classes do cardapio. Aplicar herança

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from models.restaurante import Restaurante
+from models.cardapio.bebida import Bebida
+from models.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça','gourmet')
+bebida_suco = Bebida('Suco de Abacaxi',5,'grande')
+prato_salgado = Prato('Coxinha',8,'Coxinha de Frango')
 
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Caike', 5)
+# restaurante_praca.receber_avaliacao('Gui', 10)
+# restaurante_praca.receber_avaliacao('Lais', 8)
+# restaurante_praca.receber_avaliacao('Caike', 5)
 
-restaurante_mexicano = Restaurante('Mexican Food','mexicana')
-restaurante_japones = Restaurante('japin','japonesa')
+# restaurante_mexicano = Restaurante('Mexican Food','mexicana')
+# restaurante_japones = Restaurante('japin','japonesa')
 
-restaurante_mexicano.alternar_estado()
+# restaurante_mexicano.alternar_estado()
 
 def main():
-    Restaurante.listar_restaurantes()
+    # Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_salgado)
+
 
 if __name__ == '__main__':
     main()
---
+++ models/cardapio/item_cardapio.py
@@ -0,0 +1,7 @@
+'''Aqui já é conteúdo do curso Python: avance na orientação a objetos e consuma uma api'''
+
+class ItemCardapio:
+    def __init__(self, nome, preco):
+        self._nome = nome
+        self._preco = preco
+        
